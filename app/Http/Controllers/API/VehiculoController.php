8fe6e56bb54cf96ab1048adc7d7d98ee97a4115a
soley <cd.marco.0106@gmail.com>
2022-08-31 15:36:24 -0500

    ApirestGetVehiculosConsulta

--- app/Http/Controllers/API/VehiculoController.php
+++ app/Http/Controllers/API/VehiculoController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\Resource;
+use App\Http\Resources\VehiculoResource;
 use App\Models\Vehiculo;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class VehiculoController extends Controller
 {
     /**
@@ -15,7 +17,15 @@ class VehiculoController extends Controller
      */
     public function index()
     {
-        return Vehiculo::all();
+        return VehiculoResource::collection(Vehiculo::all());
+
+        //$sql = 'SELECT * FROM vehiculos';
+        //$sql = 'SELECT marca FROM vehiculos GROUP by marca;';
+        //$products = DB::select($sql);
+        //return $products;
+
+         
+
     }
 
     /**
@@ -35,11 +45,19 @@ class VehiculoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Vehiculo $vehiculo){
-        return response()->json([
-            'res' => true,
-            'vehiculo' => $vehiculo
-        ]);
+    public function show($id){
+        //print($vehiculo);
+        //return new VehiculoResource($vehiculo);
+
+        //$sql = 'SELECT * FROM vehiculos WHERE delantera LIKE "%?%" OR trasera LIKE "%?%"',[$id];
+        //$products = DB::select('SELECT * FROM vehiculos WHERE delantera LIKE "%?%" OR trasera LIKE "%?%"',[$id,$id]);
+        $column = 'delantera';
+
+        $products = DB::select("select * from vehiculos where delantera LIKE ? OR trasera LIKE ?",['%'.$id.'%','%'.$id.'%']);
+
+        
+        
+        return $products;
         //
     }
 
