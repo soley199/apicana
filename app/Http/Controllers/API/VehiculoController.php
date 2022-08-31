0e10eb8147f24aaef2b0d83fe47435a0778dd651
soley <cd.marco.0106@gmail.com>
2022-08-31 10:23:19 -0500

    ApirestGetVehiculosPorId

--- app/Http/Controllers/API/VehiculoController.php
+++ app/Http/Controllers/API/VehiculoController.php
@@ -35,8 +35,11 @@ class VehiculoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show(Vehiculo $vehiculo){
+        return response()->json([
+            'res' => true,
+            'vehiculo' => $vehiculo
+        ]);
         //
     }
 
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('vehiculo', [VehiculoController::Class,'index']);
+Route::get('vehiculo/{vehiculo}', [VehiculoController::Class,'show']);
